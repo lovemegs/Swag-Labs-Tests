c31cab06e1c7aacc359e223dddf13cc687fc7b53
lovemegs <lovemegs94@hotmail.com>
2025-03-25 09:44:23 -0600

    Removed comment

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -1,9 +1,6 @@
 import { $ } from '@wdio/globals'
 import Page from './page.js';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class HomePage extends Page {
     /**
      * define selectors using getter methods
